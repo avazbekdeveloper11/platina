2baec0f79f6ce910cda458681457482d16154800
avazbekdeveloper11 <avazbekdeveloper11@gmail.com>
2023-08-21 15:02:04 +0500

    plantina task completed

--- lib/infrastructure/apis/home_apis.chopper.dart
+++ lib/infrastructure/apis/home_apis.chopper.dart
@@ -1,5 +1,7 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
+// ignore_for_file: duplicate_ignore
+
 part of 'home_apis.dart';
 
 // **************************************************************************
